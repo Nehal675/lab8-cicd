2e0301fe077fb89125fb800e337b5fb6d275e8c9
Ubuntu <ubuntu@ip-172-31-31-11.ec2.internal>
2026-04-07 11:47:29 +0000

    Add Tea to task list

--- init.js
+++ init.js
@@ -2,5 +2,5 @@ db = db.getSiblingDB('tasks');
 
 db.tasks.insertMany([
   { id: 1, name: "Task 1", status: "pending" },
-  { id: 2, name: "Task 2", status: "done" }
+  { id: 2, name: "Task 2", status: "done" } 
 ]);
